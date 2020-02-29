ce9ec987f915469c452cab0b07a70df789d061e1
root <root@ZCQ-DESKTOP.localdomain>
2020-02-29 22:35:22 +0800

    add the ssbdRandomSamplingUpdate method

--- src/main/java/com/ssbd/algorithms/randomsampling/RandomSamplingDemo.java
+++ src/main/java/com/ssbd/algorithms/randomsampling/RandomSamplingDemo.java
@@ -17,17 +17,15 @@
  */
 package com.ssbd.algorithms.randomsampling;
 
+import org.apache.commons.math3.random.RandomDataGenerator;
 import org.apache.flink.api.common.functions.FilterFunction;
-import org.apache.flink.api.common.operators.Order;
 import org.apache.flink.api.java.DataSet;
 import org.apache.flink.api.java.ExecutionEnvironment;
 import org.apache.flink.api.java.utils.MultipleParameterTool;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
-import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * @ClassName RandomSamplingDemo
@@ -47,64 +45,64 @@ public class RandomSamplingDemo {
         // make parameters available in the web interface
         env.getConfig().setGlobalJobParameters(params);
 
+        // create a set generator
         DataSet<Long> setGenerator = env.generateSequence(1, 10000).setParallelism(1);
 
-        DataSet<Long> set1 = setGenerator.filter(new FilterFunction<Long>() {
-            @Override
-            public boolean filter(Long aLong) throws Exception {
-                return aLong % 2 == 1;
-            }
-        });
+        // create a set1, which only contains odd numbers from 1 to 9999
+        DataSet<Long> set1 = setGenerator.filter((FilterFunction<Long>) aLong -> aLong % 2 == 1);
 
-        DataSet<Long> set2 = setGenerator.filter(new FilterFunction<Long>() {
-            @Override
-            public boolean filter(Long aLong) throws Exception {
-                return aLong % 2 == 0;
-            }
-        });
+        // create a set2, which only contains even numbers from 2 to 10000
+        DataSet<Long> set2 = setGenerator.filter((FilterFunction<Long>) aLong -> aLong % 2 == 0);
 
-//        List<Long> set1List = set1.collect();
-//        List<Long> set2List = set2.collect();
-//
-//        List<Long> samplesOfSet1 = new ArrayList<>();
-//
-//        samplesOfSet1.add(1L);
-//        samplesOfSet1.add(2L);
-//        for( Long a = 3L; a < 10000L; a++) {
-//            samplesOfSet1.add(a);
-//        }
-//
-//        DataSet<Long> summaryOfSet1 = env.fromCollection(samplesOfSet1);
+        // get summary of set1
+        List<Long> set1List = set1.collect();
+        List<Long> summaryOfSet1List = ssbdRandomSamplingUpdate(100, set1List);
+        DataSet<Long> summaryOfSet1 = env.fromCollection(summaryOfSet1List);
 
-        DataSet<Long> summaryOfSet1 = set1.filter(new FilterFunction<Long>() {
-            @Override
-            public boolean filter(Long aLong) throws Exception {
+        // get summary of set2
+        List<Long> set2List = set2.collect();
+        List<Long> summaryOfSet2List = ssbdRandomSamplingUpdate(100, set2List);
+        DataSet<Long> summaryOfSet2 = env.fromCollection(summaryOfSet2List);
 
-                return false;
-            }
-        })
+        // Merge summaries of set1 and set2
 
+
+        // print the result
         summaryOfSet1.print();
-
-        System.out.println("summary of set1: " + summaryOfSet1.count());
-
-        Thread.sleep(30000);
-//        set1List.forEach(item -> {
-//            if((item.intValue() + 1) % 10 == 0) {
-//                System.out.println("");
-//            }
-//            System.out.print(item + " ");
-//        });
-
-//        System.out.println(set1list);
-
-//        set1.print();
-//        set2.print();
-
-
+        System.out.println("summary number of set1: " + summaryOfSet1.count());
+        System.out.println("summary number of set2: " + summaryOfSet2.count());
 
 
         // execute program
 //        env.execute("Random Sampling Algorithm Implementation Demo");
     }
+
+    public static List<Long> ssbdRandomSamplingUpdate(int sampleNum, List<Long> setA) {
+        // check the input parameters
+        if (sampleNum > setA.size()) {
+            throw new IllegalArgumentException("sample number should be smaller than the cardinality of setA");
+        }
+
+        // initiate the sample set based on the original set
+        List<Long> setS = new ArrayList<>();
+        // sample set range [0, sampleNum)
+        for (int index = 0; index < sampleNum; index++) {
+            setS.add(setA.get(index));
+        }
+
+        // update the sample set when the original set fulfill the sample set
+        Random random = new Random();
+        // num range [sampleNum, setA.size)
+        for (int num = sampleNum; num < setA.size(); num++) {
+            // i range [0, num)
+            int i = random.nextInt(num);
+            if (i < sampleNum) {
+                // i range [0, sampleNum)
+                setS.set(i, setA.get(num));
+            }
+        }
+
+        return setS;
+    }
+
 }
